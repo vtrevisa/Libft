bcd0531c108606fa5cbf3458e01efb1480ca7de5
vtrevisa <vitor.artes1@hotmail.com>
2022-08-17 19:17:48 -0300

    modification libft.h, separation in Part 1 and Part 2

--- Include/libft.h
+++ Include/libft.h
@@ -6,7 +6,7 @@
 /*   By: vitor <vitor@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 23:14:48 by vtrevisa          #+#    #+#             */
-/*   Updated: 2022/06/26 15:31:58 by vitor            ###   ########.fr       */
+/*   Updated: 2022/08/17 19:03:04 by vitor            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+/* ---PART 1--- */
 int		ft_atoi(const char *str);
 
 void	ft_bzero(void *s, size_t n);
@@ -32,8 +33,6 @@ int		ft_isdigit(int c);
 
 int		ft_isprint(int c);
 
-char	*ft_itoa(int n);
-
 void	*ft_memchr(const void *s, int c, size_t n);
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
@@ -43,6 +42,30 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 
 void	*ft_memset(void *s, int c, size_t n);
+
+char	*ft_strchr(const char *s, int c);
+
+char	*ft_strdup(const char *s);
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+
+size_t	ft_strlen(const char *s);
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+
+char	*ft_strrchr(const char *s, int c);
+
+int		ft_tolower(int c);
+
+int		ft_toupper(int c);
+
+/* ---part 2--- */
+
+char	*ft_itoa(int n);
 
 void	ft_putchar_fd(char c, int fd);
 
@@ -54,35 +77,16 @@ void	ft_putstr_fd(char *s, int fd);
 
 char	**ft_split(char const *s, char c);
 
-char	*ft_strchr(const char *s, int c);
-
-char	*ft_strdup(const char *s);
-
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 char	*ft_strjoin(char const *s1, char const *s2);
 
-size_t	ft_strlcat(char *dest, const char *src, size_t size);
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-
-size_t	ft_strlen(const char *s);
-
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-
-char	*ft_strnstr(const char *big, const char *little, size_t len);
-
-char	*ft_strrchr(const char *s, int c);
 
 char	*ft_strtrim(char const *s1, char const *set);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
-int		ft_tolower(int c);
-
-int		ft_toupper(int c);
 
 /* BONUS PART*/
 
